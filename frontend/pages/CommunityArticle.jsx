18a2d4165ebef636fe6fb104998cf217b4cc98ab
adityaverma3010 <adityaverma.bsspcw@gmail.com>
2026-05-29 12:55:15 +0530

    error solved

--- frontend/pages/CommunityArticle.jsx
+++ frontend/pages/CommunityArticle.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import "../CSS/communityArticle.css";
+import "../CSS/communityarticle.css";
 
 const CommunityArticle = () => {
     const { id } = useParams();
