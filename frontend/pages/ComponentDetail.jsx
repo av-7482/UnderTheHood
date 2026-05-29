dcdc21e418e84d39923ba9ceb22e3769c7c7113e
adityaverma3010 <adityaverma.bsspcw@gmail.com>
2026-05-29 12:57:00 +0530

    error solved again

--- frontend/pages/ComponentDetail.jsx
+++ frontend/pages/ComponentDetail.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import "../CSS/Componentdetail.css";
+import "../CSS/componentdetail.css";
 
 export default function ComponentDetails() {
     const { id } = useParams();
