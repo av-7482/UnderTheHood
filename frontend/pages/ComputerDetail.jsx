893e2b61194abc88c82053633582e8cc8a368523
adityaverma3010 <adityaverma.bsspcw@gmail.com>
2026-05-29 13:00:37 +0530

    error solved again and again

--- frontend/pages/ComputerDetail.jsx
+++ frontend/pages/ComputerDetail.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import "../CSS/ComputerDetail.css";
+import "../CSS/Computerdetail.css";
 
 export default function ComputerDetails() {
     const { id } = useParams();
